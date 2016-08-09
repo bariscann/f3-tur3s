671c2c187a35a7fb45397c0227376ec4a193513c
Baris Can <bc.brscn@gmail.com>
2016-08-09 14:31:36 +0300

    bakalim olacak mi

--- heal.c
+++ heal.c
@@ -3,6 +3,7 @@
 
 #define DUZELTME_ORANI 3
 #define THRESHOLD 5
+#define ARTI_EKSI 10
 
 int main()	
 {
@@ -17,15 +18,16 @@ int main()
 	int maxKordinat2;
 	int maxFrekans1;
 	int maxFrekans2;
-	int maxKontrol;
+	int max2Kontrol;
 	int tmpFrekans;
 	int tahminiDeger[];
 	int duzeltilenDeger[];
 
 
 	i = 1;
-	while(i<tahminSayisi)
+	while((i+aralik)<tahminSayisi)
 	{
+		baslangic = i-1;
 		max1 = tahminiDeger[i-1];
 		max2 = tahminiDeger[i-1];
 		maxFrekans1 = 1;
@@ -59,8 +61,105 @@ int main()
 				tmpFrekans = 1;
 			}
 			i++;
+		}	
+
+		if((maxKordinat2 + ARTI_EKSI) >=  i)
+		{
+			j = maxKordinat2+1;
+			max2Kontrol = 0;
+			while((j < tahminSayisi) && (j < (j+2*ARTI_EKSI))
+			{
+				if(max2 == tahminiDeger[j])
+				{
+					max2Kontrol++;
+				}
+				j++;
+			}
+			if(max2Kontrol > THRESHOLD)
+			{
+				j = maxKordinat2-maxFrekans2;
+				max2Kontrol = THRESHOLD;
+				while((max2Kontrol >= THRESHOLD )
+				{
+					max2Kontrol = 0;
+					for(k = j; k > j-ARTI_EKSI; k--)
+					{
+						if(tahminiDeger[k]==max2)
+						{
+							max2Kontrol++;
+						}
+					}
+					j -= ARTI_EKSI;
+				}
+				j += ARTI_EKSI;
+				for(k = j; k < i; k++)
+				{
+					duzeltilenDeger[k] = max2;
+				}
+				for(k = baslangic; k < j; k++)
+				{
+					duzeltilenDeger[k] = max1;
+				}
+			}
 		}
-		
+		else
+		{
+			if((maxKordinat2 - maxFrekans2 - ARTI_EKSI) <=  baslangic)
+			{
+				j = (maxKordinat2 - maxFrekans2 - ARTI_EKSI);
+				
+				if(j < 0)
+				{
+					j = 0;	
+				}
+				
+				max2Kontrol = 0;
+				while(j <= (maxKordinat2-maxFrekans2))
+				{
+					if(max2 == tahminiDeger[j])
+					{
+						max2Kontrol++;
+					}
+					j++;
+				}
+
+
+				if(max2Kontrol > THRESHOLD)
+				{
+					j = maxKordinat2;
+					max2Kontrol = THRESHOLD;
+					while((max2Kontrol >= THRESHOLD )
+					{
+						max2Kontrol = 0;
+						for(k = j; k < j+ARTI_EKSI; k++)
+						{
+							if(tahminiDeger[k]==max2)
+							{
+								max2Kontrol++;
+							}
+						}
+						j += ARTI_EKSI;
+					}
+					j -= ARTI_EKSI;
+					for(k = baslangic; k < j; k++)
+					{
+						duzeltilenDeger[k] = max2;
+					}
+					for(k = j; k < i; k++)
+					{
+						duzeltilenDeger[k] = max1;
+					}
+				}
+			}
+			else
+			{
+				for(k = baslangic; k < i; k++)
+					{
+						duzeltilenDeger[k] = max1;
+					}
+			}	
+		}
+		i++;
 	}
 
 	for(i = 0; i < tahminSayisi; i+=aralik)
